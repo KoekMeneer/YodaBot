50ff1e6793321d8f24524adec407c1a7bd025135
KoekMeneer <ricklugtigheid@protonmail.com>
2022-07-05 14:20:45 +0200

    Added arguments for setting the process to start

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const args = process.argv.slice(2);
+
 // Start our discord bot
 //
-require('./src/index')();
+if (args.length == 0 || args.includes('bot'))
+{
+    require('./src/index')();
+}
 
 // Start our webiste
 //
-require('./web/index')();
+if (args.length == 0 || args.includes('web'))
+{
+    require('./web/index')();
+}
